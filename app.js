9346e248a76bd6f6b8b0df0202fd4cb016e73731
Gourav Kumar <2023kucp1029@iiitkota.ac.in>
2025-06-19 17:41:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -175,4 +175,7 @@ app.get('/tournament/:name', (req, res) => {
     }
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
